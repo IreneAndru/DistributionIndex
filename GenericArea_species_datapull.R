3297af5c4f0e8ddffe1cebc2785dd73541e427bd
IreneAndru <irene.andrushchenko@dfo-mpo.gc.ca>
2022-01-18 12:10:21 -0400

    Included all of the possible area combinations for Pollock.

--- GenericArea_species_datapull.R
+++ GenericArea_species_datapull.R
@@ -1,7 +1,7 @@
 ###Reworking of Dan Ricard's code (https://github.com/dfo-gulf-science/Maritimes-SUMMER-Atlas) to apply to various pollock spatial units.
 
 #Options:
-#Which spatial area do you want this dataset to apply to? Options: WC, 4X5Y, 4X5, 5Z, EC, 4XVW5
+#Which spatial area do you want this dataset to apply to? Options: WC, 4X5Y, 4X5, 5Z, 4Xmn, EC, 4XVW5, 4VW
 
 Assessment_strata<-'WC'
 
@@ -184,13 +184,17 @@ return(merged.df)
 pollock.df<-extract.catch.fct(16)
 ##############
 
-#Which spatial area do you want this dataset to apply to? Options: WC, 4X5Y, 4X5, 5Z, EC, 4XVW5
-if(Assessment_strata=='WC'){pollock.df<-subset(pollock.df, strat %in% c('474','476','480','481','482','483','484','485','490','491','492','493','494','495'))}
-if(Assessment_strata=='4X5'){pollock.df<-subset(pollock.df, strat %in% c('474','476','480','481','482','483','484','485','490','491','492','493','494','495','5Z1','5Z2','5Z9'))}
-if(Assessment_strata=='5Z'){pollock.df<-subset(pollock.df, strat %in% c('5Z1','5Z2','5Z9'))}
+#Which spatial area do you want this dataset to apply to? Options: WC, 4X5Y, 4X5, 5Z, EC, 4XVW5, 4VW
+if(Assessment_strata=='4XVW5'){pollock.df<-pollock.df} #includes everything
+if(Assessment_strata=='WC'){pollock.df<-subset(pollock.df, strat %in% c('474','476','480','481','482','483','484','485','490','491','492','493','494','495','5Z1','5Z2','5Z9'))} #excludes 4Xmn, includes EGB
+if(Assessment_strata=='4X5'){pollock.df<-subset(pollock.df, strat %in% c('470','471','472','473','474','475','476','480','481','482','483','484','485','490','491','492','493','494','495','5Z1','5Z2','5Z9'))} #incudes 4Xmn and EGB
+if(Assessment_strata=='4X5Y'){pollock.df<-subset(pollock.df, strat %in% c('470','471','472','473','474','475','476','480','481','482','483','484','485','490','491','492','493','494','495'))} #incudes 4Xmn but not EGB
+if(Assessment_strata=='5Z'){pollock.df<-subset(pollock.df, strat %in% c('5Z1','5Z2','5Z9'))} #Only EGB
+if(Assessment_strata=='4Xmn'){pollock.df<-subset(pollock.df, strat %in% c('470','471','472','473','475','477','478')}#only 4Xmn
+if(Assessment_strata=='EC'){pollock.df<-subset(pollock.df, strat %in% c('558','559','440','441','442','443','444','445','446','447','448','449','450','451','452','453','454','455','456','457','458','459','460','461','462','463','464','465','466','470','471','472','473','475','477','478'))} #incudes 4Xmn
+if(Assessment_strata=='4VW'){pollock.df<-subset(pollock.df, strat %in% c('558','559','440','441','442','443','444','445','446','447','448','449','450','451','452','453','454','455','456','457','458','459','460','461','462','463','464','465','466'))} #incudes 4Xmn
 
-#Left to code:
-#4X5Y, EC, 4XVW5
+
 
 
 
